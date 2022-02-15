57caf0cb75158bc08b4c0e91cb4552155cd05cc3
samuel <partogusamuel07@gmail.com>
2022-02-15 20:42:58 +0700

    Change to the color and name

--- everblush-theme.el
+++ everblush-theme.el
@@ -1,6 +1,4 @@
-;;; uwu-theme.el --- based on UwU colorscheme by Mangeshrex -*- lexical-binding: t; no-byte-compile: t; -*-
-;Author: SamuelNhBos
-
+;;; everblush-theme.el --- based on everblush colorscheme by Mangeshrex -*- lexical-binding: t; no-byte-compile: t; -*-
 ;;; Code:
 
 (require 'doom-themes)
@@ -9,69 +7,69 @@
 ;;
 ;;; Variables
 
-(defgroup uwu-theme nil
+(defgroup everblush-theme nil
   "Options for the `doom-one' theme."
   :group 'doom-themes)
 
-(defcustom uwu-brighter-modeline nil
+(defcustom everblush-brighter-modeline nil
   "If non-nil, more vivid colors will be used to style the mode-line."
-  :group 'uwu-theme
+  :group 'everblush-theme
   :type 'boolean)
 
-(defcustom uwu-brighter-comments nil
+(defcustom everblush-brighter-comments nil
   "If non-nil, comments will be highlighted in more vivid colors."
-  :group 'uwu-theme
+  :group 'everblush-theme
   :type 'boolean)
 
-(defcustom uwu-padded-modeline doom-themes-padded-modeline
+(defcustom everblush-padded-modeline doom-themes-padded-modeline
   "If non-nil, adds a 4px padding to the mode-line.
 Can be an integer to determine the exact padding."
-  :group 'uwu-theme
+  :group 'everblush-theme
   :type '(choice integer boolean))
 
 
 ;;
 ;;; Theme definition
 
-(def-doom-theme uwu
+(def-doom-theme everblush
   "A beautiful and dark vim colorscheme ported to doom emacs."
 
   ;; name        default   256           16
-  ((bg         '("#131a1c" "#131a1c"      nil  )) ;
-   (fg         '("#c5c8c9" "#c5c8c9"    "brightwhite"  )) ;
+  ((bg         '("#181f21" "#181f21"      nil  )) ;
+   (fg         '("#dadada" "#dadada"    "brightwhite"  )) ;
 
    ;; These are off-color variants of bg/fg, used primarily for `solaire-mode',
    ;; but can also be useful as a basis for subtle highlights (e.g. for hl-line
    ;; or region), especially when paired with the `doom-darken', `doom-lighten',
    ;; and `doom-blend' helper functions.
-   (bg-alt     '("#131a1c" "#131a1c"     nil        )) ;
-   (fg-alt     '("#c5c8c9" "#c5c8c9"    "white"        )) ;
+   (bg-alt     '("#181f21" "#181f21"     nil        )) ;
+   (fg-alt     '("#dadada" "#dadada"    "white"        )) ;
 
    ;; These should represent a spectrum from bg to fg, where base0 is a starker
    ;; bg and base8 is a starker fg. For example, if bg is light grey and fg is
    ;; dark grey, base0 should be white and base8 should be black.
-   (base0      '("#131a1c" "#131a1c"     "black"        )) ;
-   (base1      '("#1b2224" "#1b2224"     "brightblack"  )) ;
-   (base2      '("#1b2224" "#1b2224"     "brightblack"  )) ;
-   (base3      '("#232a2c" "#232a2c"     "brightblack"  )) ;
-   (base4      '("#2f3638" "#2f3638"     "brightblack"  )) ;
-   (base5      '("#2f3638" "#2f3638"     "brightblack"  )) ;
-   (base6      '("#232a2c" "#232a2c"     "brightblack"  )) ;
-   (base7      '("#232a2c" "#232a2c"     "brightblack"  )) ;
-   (base8      '("#c4c4c4" "#c4c4c4"     "white"        )) ;
+   (base0      '("#181f21" "#181f21"     "black"        )) ;
+   (base1      '("#b3b9b8" "#b3b9b8"     "brightblack"  )) ;
+   (base2      '("#b3b9b8" "#b3b9b8"     "brightblack"  )) ;
+   (base3      '("#22292b" "#22292b"     "brightblack"  )) ;
+   (base4      '("#363d3f" "#363d3f"     "brightblack"  )) ;
+   (base5      '("#363d3f" "#363d3f"     "brightblack"  )) ;
+   (base6      '("#22292b" "#22292b"     "brightblack"  )) ;
+   (base7      '("#22292b" "#22292b"     "brightblack"  )) ;
+   (base8      '("#bcd3c2" "#bcd3c2"     "white"        )) ;
 
    (grey       base4)
-   (red        '("#f65b5b" "#f65b5b" "red"          ))
-   (orange     '("#e59e67" "#e59e67" "brightred"    ))
-   (green      '("#6bb05d" "#6bb05d" "green"        ))
-   (teal       '("#8dc776" "#8dc776" "brightgreen"  ))
-   (yellow     '("#e59e67" "#e59e67" "yellow"       ))
-   (blue       '("#6cbad1" "#6cbad1" "brightblue"   ))
-   (dark-blue  '("#53a7bf" "#53a7bf" "blue"         ))
-   (magenta    '("#bb8fe5" "#bb8fe5" "brightmagenta"))
-   (violet     '("#b185db" "#b185db" "magenta"      ))
-   (cyan       '("#6db0ad" "#6db0ad" "brightcyan"   ))
-   (dark-cyan  '("#51a39f" "#51a39f" "cyan"         ))
+   (red        '("#e06e6e" "#e06e6e" "red"          ))
+   (orange     '("#ef7d7d" "#ef7d7d" "brightred"    ))
+   (green      '("#8ccf7e" "#8ccf7e" "green"        ))
+   (teal       '("#8ad8ef" "#8ad8ef" "brightgreen"  ))
+   (yellow     '("#d8b25b" "#d8d25b" "yellow"       ))
+   (blue       '("#5db7cb" "#5db7cb" "brightblue"   ))
+   (dark-blue  '("#5dc1dd" "#5dc1dd" "blue"         ))
+   (magenta    '("#c47fd5" "#c47fd5" "brightmagenta"))
+   (violet     '("#bab3e5" "#bab3e5" "magenta"      ))
+   (cyan       '("#77aed7" "#77aed7" "brightcyan"   ))
+   (dark-cyan  '("#6da4cd" "#51a39f" "cyan"         ))
 
    ;; These are the "universal syntax classes" that doom-themes establishes.
    ;; These *must* be included in every doom themes, or your theme will throw an
@@ -80,19 +78,19 @@ Can be an integer to determine the exact padding."
    (vertical-bar   (doom-darken base1 0.1))
    (selection      dark-blue)
    (builtin        magenta)
-   (comments       (if uwu-brighter-comments dark-cyan base5))
-   (doc-comments   (doom-lighten (if uwu-brighter-comments dark-cyan base5) 0.25))
+   ;;(comments       (if everblush-brighter-comments dark-cyan base5))
+   (comments       grey)
+   (doc-comments   (doom-lighten (if everblush-brighter-comments dark-cyan base5) 0.25))
    (constants      violet)
-;;   (functions      magenta)
    (functions      orange)
-   (keywords       blue)
+   (keywords       teal)
    (methods        cyan)
    (operators      blue)
    (type           yellow)
    (strings        green)
-;;   (variables      (doom-lighten magenta 0.4))
-   (variables      dark-blue)
-   (numbers        orange)
+   (variables      orange)
+;;   (variables      dark-blue)
+   (numbers        dark-cyan)
    (region         `(,(doom-lighten (car bg-alt) 0.15) ,@(doom-lighten (cdr base1) 0.35)))
    (error          red)
    (warning        yellow)
@@ -105,39 +103,39 @@ Can be an integer to determine the exact padding."
    ;; mandatory for derived themes.
    (modeline-fg              fg)
    (modeline-fg-alt          base5)
-   (modeline-bg              (if uwu-brighter-modeline
+   (modeline-bg              (if everblush-brighter-modeline
                                  (doom-darken blue 0.45)
                                (doom-darken bg-alt 0.1)))
-   (modeline-bg-alt          (if uwu-brighter-modeline
+   (modeline-bg-alt          (if everblush-brighter-modeline
                                  (doom-darken blue 0.475)
                                `(,(doom-darken (car bg-alt) 0.15) ,@(cdr bg))))
    (modeline-bg-inactive     `(,(car bg-alt) ,@(cdr base1)))
    (modeline-bg-inactive-alt `(,(doom-darken (car bg-alt) 0.1) ,@(cdr bg)))
 
    (-modeline-pad
-    (when uwu-padded-modeline
-      (if (integerp uwu-padded-modeline) uwu-padded-modeline 4))))
+    (when everblush-padded-modeline
+      (if (integerp everblush-padded-modeline) everblush-padded-modeline 4))))
 
 
   ;;;; Base theme face overrides
   (((line-number &override) :foreground base4)
    ((line-number-current-line &override) :foreground fg)
    ((font-lock-comment-face &override)
-    :background (if uwu-brighter-comments (doom-lighten bg 0.05)))
+    :background (if everblush-brighter-comments (doom-lighten bg 0.05)))
    (mode-line
     :background modeline-bg :foreground modeline-fg
     :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg)))
    (mode-line-inactive
     :background modeline-bg-inactive :foreground modeline-fg-alt
     :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-inactive)))
-   (mode-line-emphasis :foreground (if uwu-brighter-modeline base8 highlight))
+   (mode-line-emphasis :foreground (if everblush-brighter-modeline base8 highlight))
 
    ;;;; css-mode <built-in> / scss-mode
    (css-proprietary-property :foreground orange)
    (css-property             :foreground green)
    (css-selector             :foreground blue)
    ;;;; doom-modeline
-   (doom-modeline-bar :background (if uwu-brighter-modeline modeline-bg highlight))
+   (doom-modeline-bar :background (if everblush-brighter-modeline modeline-bg highlight))
    (doom-modeline-buffer-file :inherit 'mode-line-buffer-id :weight 'bold)
    (doom-modeline-buffer-path :inherit 'mode-line-emphasis :weight 'bold)
    (doom-modeline-buffer-project-root :foreground green :weight 'bold)
@@ -167,4 +165,4 @@ Can be an integer to determine the exact padding."
   ;;;; Base theme variable overrides-
   ())
 
-;;; doom-one-theme.el ends here
+;;; everblush-theme.el ends here
